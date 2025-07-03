8b671d9775a0b19a0bed2d58c3832aec7c31d29a
Sabrina Arevalo <xsabrinarvlx@gmail.com>
2025-07-03 14:45:12 -0300

    WINER_COMBOS

--- 02-Tic-tac-toe/src/App.jsx
+++ 02-Tic-tac-toe/src/App.jsx
@@ -45,6 +45,17 @@ function App() {
     const newTurn = turn === TURN.X ? TURN.O : TURN.X
     setTurn(newTurn)
   }
+
+  const WINER_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
   
   return (
     
